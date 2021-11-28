987e0284e522d908b4819d7b5a60d6f0edbb0d87
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-11-28 22:08:02 +0800

    matrix: Implement initial matrix representation

---
+++ src/matrix.h
@@ -0,0 +1,30 @@
+#ifndef RAYTRACER_MATRIX_H
+#define RAYTRACER_MATRIX_H
+
+#include <array>
+#include <cstddef>
+
+template<class T, size_t N>
+class Matrix {
+public:
+    constexpr Matrix(const std::initializer_list<T> &data_) {
+        std::copy_n(begin(data_), std::min(std::size(data), std::size(data_)), begin(data));
+    }
+
+    [[nodiscard]] constexpr T &operator[](const std::array<size_t, 2> &idx) {
+        return data[index(idx[0], idx[1])];
+    }
+
+    [[nodiscard]] constexpr const T &operator[](const std::array<size_t, 2> &idx) const {
+        return data[index(idx[0], idx[1])];
+    }
+
+private:
+    std::array<T, N * N> data;
+
+    [[nodiscard]] constexpr size_t index(size_t r, size_t c) const {
+        return r * N + c;
+    }
+};
+
+#endif //RAYTRACER_MATRIX_H
---
+++ src/test/matrix.cpp
@@ -0,0 +1,46 @@
+#include "matrix.h"
+#include "numeric.h"
+
+#include <catch2/catch.hpp>
+
+SCENARIO("Constructing and inspecting a 4x4 matrix") {
+    GIVEN("A matrix M and some definitions") {
+        const Matrix<float, 4> M = {1.0f, 2.0f, 3.0f, 4.0f, 5.5f, 6.5f, 7.5f, 8.5f, 9.0f, 10.0f, 11.0f, 12.0f, 13.5f,
+                                    14.5f, 15.5f, 16.5f};
+
+        THEN("M has the correct attributes") {
+            CHECK(equal(M[{0, 0}], 1.0f));
+            CHECK(equal(M[{0, 3}], 4.0f));
+            CHECK(equal(M[{1, 0}], 5.5f));
+            CHECK(equal(M[{1, 2}], 7.5f));
+            CHECK(equal(M[{2, 2}], 11.0f));
+            CHECK(equal(M[{3, 0}], 13.5f));
+            CHECK(equal(M[{3, 2}], 15.5f));
+        }
+    }
+}
+
+SCENARIO("A 2x2 matrix ought to be representable") {
+    GIVEN("A 2x2 matrix M") {
+        const Matrix<float, 2> M = {-3, 5, 1, -2};
+
+        THEN("M has the correct attributes") {
+            CHECK(equal(M[{0, 0}], -3));
+            CHECK(equal(M[{0, 1}], 5));
+            CHECK(equal(M[{1, 0}], 1));
+            CHECK(equal(M[{1, 1}], -2));
+        }
+    }
+}
+
+SCENARIO("A 3x3 matrix ought to be representable") {
+    GIVEN("A 3x3 matrix M") {
+        const Matrix<float, 3> M = {-3, 5, 0, 1, -2, -7, 0, 1, 1};
+
+        THEN("M has the correct attributes") {
+            CHECK(equal(M[{0, 0}], -3));
+            CHECK(equal(M[{1, 1}], -2));
+            CHECK(equal(M[{2, 2}], 1));
+        }
+    }
+}
